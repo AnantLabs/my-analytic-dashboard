22ab48ba8a800fac4c0648bfd07ebbb5ca679a2c
vinod.pahuja <vinod.pahuja@50ea1a14-fa45-a63a-8afa-e808fa6692da>
2014-09-19 18:37:55 +0000

    Initial Import

---
+++ Subscriber/src/main/java/rnd/data/subscriber/DataSubscriber.java
@@ -0,0 +1,26 @@
+package rnd.data.subscriber;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import rnd.data.router.AbstractDataRouter;
+import rnd.util.AppConfig;
+import rnd.util.JacksonUtils;
+
+public class DataSubscriber extends AbstractDataRouter {
+
+	public String getData(HttpServletRequest request, HttpServletResponse response) throws Throwable {
+
+		Map requestPayLoad = parseRequest(request);
+
+		Object responseData = processRequest(requestPayLoad, AppConfig.getProperty("APPLICATION"), "data", response, new HashMap());
+
+		if (responseData instanceof String) { return (String) responseData; }
+
+		return JacksonUtils.convertToJSON(responseData);
+	}
+
+}
