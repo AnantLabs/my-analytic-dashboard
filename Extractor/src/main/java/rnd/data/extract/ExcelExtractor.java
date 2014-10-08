3e36d87243a8033a844f3bbe1e57ddb8246788be
vinod.pahuja <vinod.pahuja@50ea1a14-fa45-a63a-8afa-e808fa6692da>
2014-10-08 05:56:57 +0000

    Initial import

---
+++ Extractor/src/main/java/rnd/data/extract/ExcelExtractor.java
@@ -0,0 +1,19 @@
+package rnd.data.extract;
+
+import rnd.data.process.AbstractDataProcessor;
+
+public class ExcelExtractor extends AbstractDataProcessor {
+
+	@Override
+	public Object processRequest(Object requestPayLoad, DataProcessorCallback callback) throws Throwable {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object processResponse(Object responseData, Object requestPayLoad, Object responsePayLoad, DataProcessorCallback callback) throws Throwable {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
