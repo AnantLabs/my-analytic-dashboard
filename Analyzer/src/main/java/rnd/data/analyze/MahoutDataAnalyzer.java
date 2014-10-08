d176d0df95967dbbc154e3f9a4c2019b1dc04162
vinod.pahuja <vinod.pahuja@50ea1a14-fa45-a63a-8afa-e808fa6692da>
2014-10-08 05:55:30 +0000

    Initial import

---
+++ Analyzer/src/main/java/rnd/data/analyze/MahoutDataAnalyzer.java
@@ -0,0 +1,18 @@
+package rnd.data.analyze;
+
+import rnd.data.process.AbstractDataProcessor;
+
+@SuppressWarnings("unchecked")
+public class MahoutDataAnalyzer extends AbstractDataProcessor {
+
+	@Override
+	public Object processRequest(Object requestPayLoad, DataProcessorCallback callback) throws Throwable {
+		return null;
+	}
+
+	@Override
+	public Object processResponse(Object responseData, Object requestPayLoad, Object responsePayLoad, DataProcessorCallback callback) throws Throwable {
+		return null;
+	}
+
+}
