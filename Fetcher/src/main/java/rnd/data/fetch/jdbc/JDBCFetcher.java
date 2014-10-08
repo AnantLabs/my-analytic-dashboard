f09fb85c68a730cc03dcdf8bfdc897fd7c92b841
vinod.pahuja <vinod.pahuja@50ea1a14-fa45-a63a-8afa-e808fa6692da>
2014-10-08 05:58:26 +0000

    Initial import

---
+++ Fetcher/src/main/java/rnd/data/fetch/jdbc/JDBCFetcher.java
@@ -0,0 +1,127 @@
+package rnd.data.fetch.jdbc;
+
+import java.util.List;
+import java.util.Map;
+
+import rnd.data.fetch.jdbc.JDBCInfo.Type;
+import rnd.data.process.AbstractDataProcessor;
+import rnd.util.StringUtils;
+
+@SuppressWarnings("unchecked")
+public class JDBCFetcher extends AbstractDataProcessor<JDBCInfo, JDBCResponsePayLoad> {
+
+	@Override
+	public String processRequest(JDBCInfo jdbcInfo, DataProcessorCallback callback) throws Throwable {
+
+		if (callback != null) {
+			callback.processCallback(new JDBCFetcherCallbackContext(jdbcInfo, null));
+		}
+
+		String sqlStmt = jdbcInfo.getStmt();
+		Map payLoad = jdbcInfo.getPayLoad();
+		Map<String, List<Map<String, String>>> paramsInfo = jdbcInfo.getParamsInfo();
+
+		sqlStmt = prepareStatement(sqlStmt, paramsInfo, payLoad);
+
+		if (jdbcInfo.getType() == Type.SP) {
+			sqlStmt = "call " + sqlStmt;
+		}
+
+		return sqlStmt;
+	}
+
+	private String prepareStatement(String sqlStmt, Map<String, List<Map<String, String>>> paramsInfo, Map payLoad) {
+
+		int startIndex = 0;
+		int endIndex = sqlStmt.indexOf('@');
+
+		if (endIndex == -1) { return sqlStmt; }
+
+		StringBuilder sqlStmtBuilder = new StringBuilder();
+
+		while (endIndex != -1) {
+
+			// Extract SQL statement
+			sqlStmtBuilder.append(sqlStmt.substring(startIndex, endIndex));
+
+			// Extract param from SQL statement
+			int paramEndIndex = sqlStmt.indexOf('@', endIndex + 1);
+			String prpName = sqlStmt.substring(endIndex + 1, paramEndIndex);
+
+			// Fetch value from data model
+			String prpValue = null;
+
+			if (paramsInfo != null) {
+				List<Map<String, String>> valueInfoList = paramsInfo.get(prpName);
+				if (valueInfoList != null) {
+					prpValue = getValue(valueInfoList, payLoad);
+				}
+			}
+
+			if (prpValue == null) {
+				prpValue = (String) payLoad.get(prpName);
+			}
+
+			// Replace param with actual data value
+			sqlStmtBuilder.append("'" + prpValue + "'");
+
+			startIndex = paramEndIndex + 1;
+			endIndex = sqlStmt.indexOf('@', startIndex);
+		}
+
+		sqlStmtBuilder.append(sqlStmt.substring(startIndex));
+
+		return sqlStmtBuilder.toString();
+	}
+
+	private String getValue(List<Map<String, String>> valueInfoList, Map payLoad) {
+
+		String prpValue = "";
+
+		for (int i = 0; i < valueInfoList.size(); i++) {
+
+			Map<String, String> valueInfo = valueInfoList.get(i);
+
+			prpValue += decorateValue(getValue(valueInfo.get("type"), valueInfo, payLoad), valueInfo);
+		}
+
+		return prpValue;
+	}
+
+	private String decorateValue(String tempPrpValue, Map<String, String> valueInfo) {
+
+		if (tempPrpValue == null) {
+			tempPrpValue = "";
+		}
+
+		String filler = valueInfo.get("fill");
+		if (filler != null) {
+			tempPrpValue = StringUtils.fill(tempPrpValue, filler, Integer.parseInt(valueInfo.get("length")));
+		}
+
+		return tempPrpValue;
+	}
+
+	protected String getValue(String type, Map<String, String> valueInfo, Map payLoad) {
+
+		String prpValue = null;
+		if (type == null) {
+			prpValue = (String) payLoad.get(valueInfo.get("name"));
+		} else if (type.equals("VALUE")) {
+			prpValue = valueInfo.get("value");
+		}
+
+		return prpValue;
+	}
+
+	@Override
+	public Object processResponse(Object responseXML, JDBCInfo jdbcInfo, JDBCResponsePayLoad jdbcResponsePayLoad, DataProcessorCallback callback) throws Throwable {
+
+		if (callback != null) {
+			callback.processCallback(new JDBCFetcherCallbackContext(jdbcInfo, jdbcResponsePayLoad));
+		}
+
+		return getDelegate().processResponse(responseXML, jdbcInfo, jdbcResponsePayLoad, getResponseProcessorCallback(jdbcInfo, jdbcResponsePayLoad));
+
+	}
+}
---
+++ Fetcher/src/main/java/rnd/data/fetch/jdbc/JDBCInfo.java
@@ -0,0 +1,62 @@
+package rnd.data.fetch.jdbc;
+
+import java.util.List;
+import java.util.Map;
+
+public class JDBCInfo {
+
+	public static enum Type {
+		SP, SQL
+	}
+
+	private Type type;
+
+	private String stmt;
+
+	private String selectInfo;
+
+	private Map<String, List<Map<String, String>>> paramsInfo;
+
+	private Map payLoad;
+
+	public Type getType() {
+		return type;
+	}
+
+	public void setType(Type type) {
+		this.type = type;
+	}
+
+	public String getStmt() {
+		return stmt;
+	}
+
+	public void setStmt(String info) {
+		this.stmt = info;
+	}
+
+	public String getSelectInfo() {
+		return selectInfo;
+	}
+
+	public void setSelectInfo(String selectInfo) {
+		this.selectInfo = selectInfo;
+	}
+
+	public void setParamsInfo(Map<String, List<Map<String, String>>> paramsInfo) {
+		this.paramsInfo = paramsInfo;
+	}
+
+	public Map<String, List<Map<String, String>>> getParamsInfo() {
+		return paramsInfo;
+	}
+
+	public Map getPayLoad() {
+		return payLoad;
+	}
+
+	public void setPayLoad(Map payLoad) {
+		this.payLoad = payLoad;
+	}
+
+}
